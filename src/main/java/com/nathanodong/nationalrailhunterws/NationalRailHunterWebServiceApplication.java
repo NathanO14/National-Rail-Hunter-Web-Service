6db8c7b4848bb0efd4f232a91047b0c10cbebd4b
Nathan Odong <NathanO14@users.noreply.github.com>
2020-07-19 12:51:34 +0100

    #1: Initial commit

---
+++ src/main/java/com/nathanodong/nationalrailhunterws/NationalRailHunterWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nathanodong.nationalrailhunterws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalRailHunterWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalRailHunterWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathanodong/nationalrailhunterws/NationalRailHunterWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanodong.nationalrailhunterws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalRailHunterWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
